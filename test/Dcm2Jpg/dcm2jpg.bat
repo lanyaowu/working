31b554692045d4ab1d3d78af39a6322c389804d7
lanyaowu <yaowulan@gmail.com>
2022-06-13 17:15:47 +0900

    delete

--- test/Dcm2Jpg/dcm2jpg.bat
+++
@@ -1,3 +0,0 @@
-cd tools\Dcm2Jpg
-rem in-dicom-file out-jpeg-file frameNo
-Dcm2JpegGenerator.exe %1 %2 %3
--- test/DcmCompress/dicomCompress.bat
+++
@@ -1,3 +0,0 @@
-cd tools\DcmCompress
-rem in-dicom-file out-dicom-file quanlity
-DcmCompress.exe %1  %2 80
